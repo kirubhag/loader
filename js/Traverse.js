ccdeabbf0bd073c881b40dc6e4c543219e664225
kirubhag <kirubhag@gmail.com>
2014-06-18 20:29:47 +0530

    Simple Traversal Algorithm Added

---
+++ js/Traverse.js
@@ -0,0 +1,21 @@
+//Reference Url http://www.mypriceindia.com/mobiles/apple/apple-iphone-5s-16gb-b95652
+var siteTable=document.createElement('table');
+var ab=document.getElementsByTagName('table');
+
+siteTable=ab[0];
+
+function getTree(node) {
+    var r = {tag: node.nodeName}, a, i;
+    if (node.childElementCount) {
+        r.children = [];
+        for (i = 0; a = node.children[i]; i++ ) {
+            r.children.push(getTree(a));
+        }
+    }
+    for (i = 0; a = node.attributes[i]; i++) {
+        r[a.nodeName] = a.nodeValue;
+    }
+    return r;        
+} 
+
+var tab=getTree(siteTable);
